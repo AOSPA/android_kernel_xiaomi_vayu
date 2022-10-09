71f4227e89aa81373601f7887abeeb24518a6e5b
raghavt20 <raghavt20@aospa.co>
2022-10-09 19:07:51 +0530

    build.sh: Switch to keep.sh for uploading kernel zips
    Change-Id: Ia503562b0a5d28eb49e9dd2611f97d940c2d732c

--- build.sh
+++ build.sh
@@ -76,7 +76,7 @@ completion()
         rm -rf $anykernel
         END=$(date +"%s")
         DIFF=$(($END - $START))
-        curl --upload-file $HOME/$zip_name http://transfer.sh/$zip_name; echo
+        curl --upload-file $HOME/$zip_name https://free.keep.sh; echo
         echo -e ${LGR} "############################################"
         echo -e ${LGR} "############# OkThisIsEpic!  ##############"
         echo -e ${LGR} "############################################${NC}"
